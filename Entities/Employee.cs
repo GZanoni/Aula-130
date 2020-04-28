e48dac538a97848865b1b821520ebc1d3287eaad
GZanoni <gean.zanoni@yahoo.com.br>
2020-04-28 20:12:59 -0300

    Projeto da aula 130 finalizado com sucesso

---
+++ Entities/Employee.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace EmployeeAndOutsourced_130.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public virtual double Payment()
+        {
+            return Hours * ValuePerHour;
+        }
+
+    }
+}
